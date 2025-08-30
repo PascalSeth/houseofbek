2065ba3d0d8f77e5e7bca4979fd763750fdbd4bd
pascalseth <pascalelikem@gmail.com>
2025-08-30 01:55:13 +0000

    build

--- app/(pages)/reservations/page.tsx
+++ app/(pages)/reservations/page.tsx
@@ -73,3 +73,14 @@
 //     </Suspense>
 //   )
 // }
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
--- app/(pages)/reservations/reservations-client.tsx
+++ app/(pages)/reservations/reservations-client.tsx
@@ -864,3 +864,14 @@
 //     </div>
 //   )
 // }
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
